68f2ade3758f3f5de9172091fe4f19ffdca29aa3
Tilman Baumann <tilman@baumann.name>
2024-03-20 15:39:23 +0000

    Try cargo2nix

--- flake.nix
+++ flake.nix
@@ -1,80 +1,28 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    systems.url = "github:nix-systems/default";
-
-    # Dev tools
-    treefmt-nix.url = "github:numtide/treefmt-nix";
+    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
+    flake-utils.follows = "cargo2nix/flake-utils";
+    nixpkgs.follows = "cargo2nix/nixpkgs";
   };
 
-  outputs = inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import inputs.systems;
-      imports = [
-        inputs.treefmt-nix.flakeModule
-      ];
-      perSystem = { config, self', pkgs, lib, system, ... }:
-        let
-          cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
-          nonRustDeps = [
-            pkgs.libiconv
-          ];
-          rust-toolchain = pkgs.symlinkJoin {
-            name = "rust-toolchain";
-            paths = with pkgs; [ rustc cargo cargo-watch rust-analyzer rustPlatform.rustcSrc ];
-          };
-        in
-        {
-          # Rust package
-          packages.default = pkgs.symlinkJoin {
-            name = "waybar_media_display";
-            paths = [
-              pkgs.playerctl
-              (
-              pkgs.rustPlatform.buildRustPackage {
-                inherit (cargoToml.package) name version;
-                src = ./.;
-                cargoLock.lockFile = ./Cargo.lock;
-                buildInputs = nonRustDeps;
-                nativeBuildInputs = with pkgs; [
-                  rust-toolchain
-                ];
-              }
-              )
-            ];
-          };
-
-          # Rust dev environment
-          devShells.default = pkgs.mkShell {
-            inputsFrom = [
-              config.treefmt.build.devShell
-            ];
-            shellHook = ''
-              # For rust-analyzer 'hover' tooltips to work.
-              export RUST_SRC_PATH=${pkgs.rustPlatform.rustLibSrc}
-
-              echo
-              echo "🍎🍎 Run 'just <recipe>' to get started"
-              just
-            '';
-            buildInputs = nonRustDeps;
-            nativeBuildInputs = with pkgs; [
-              just
-              rust-toolchain
-            ];
-            RUST_BACKTRACE = 1;
-          };
-
-          # Add your auto-formatters here.
-          # cf. https://numtide.github.io/treefmt/
-          treefmt.config = {
-            projectRootFile = "flake.nix";
-            programs = {
-              nixpkgs-fmt.enable = true;
-              rustfmt.enable = true;
-            };
-          };
+  outputs = inputs: with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [cargo2nix.overlays.default];
         };
-    };
+
+        rustPkgs = pkgs.rustBuilder.makePackageSet {
+          rustVersion = "1.75.0";
+          packageFun = import ./Cargo.nix;
+        };
+
+      in rec {
+        packages = {
+          waybar_media_display = (rustPkgs.workspace.waybar_media_display {});
+          default = packages.waybar_media_display;
+        };
+      }
+    );
 }
